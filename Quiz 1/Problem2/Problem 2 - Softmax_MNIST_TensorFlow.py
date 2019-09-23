c2091f1a4961008cd0234ca01548a9ad678d609d
sohailf7 <33504467+sohailf7@users.noreply.github.com>
2019-09-22 20:42:56 -0400

    Problem 2 file

---
+++ Quiz 1/Problem2/Problem 2 - Softmax_MNIST_TensorFlow.py
@@ -0,0 +1,59 @@
+
+import tensorflow as tf
+tf.set_random_seed(42)
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+# Display Training Data
+print('Training Data:')
+print('Number of training examples: ', mnist.train.num_examples)
+print('Shape of training inputs: ', mnist.train.images.shape)
+print('Shape of training labels: ', mnist.train.labels.shape)
+
+# Display validation data
+print('Validation Data:')
+print('Number of validation examples: ', )
+print('Shape of validation inputs: ', )
+print('Shape of validation labels: ', )
+
+print('Test Data:')
+print('Number of test examples: ', )
+print('Shape of test inputs: ', )
+print('Shape of labels labels: ', )
+
+x = tf.placeholder(tf.float32, shape=[None, 784])
+y = tf.placeholder(tf.float32, shape=[None, 10])
+
+# Initialize to zero
+W = tf.Variable(tf.zeros([784,10]))
+b = tf.Variable(tf.zeros([10]))
+
+# Hypothesis
+y_hat = tf.matmul(x,W) + b
+
+# Calculate loss
+loss_cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=y_hat))
+
+# Gradient Descent
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss_cross_entropy)
+
+# Execute the graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for _ in range(1000):
+        batch = mnist.train.next_batch(20)
+        train_step.run(feed_dict={x: batch[0], y: batch[1]})
+  
+
+    correct_prediction = tf.equal(tf.argmax(y_hat,1), tf.argmax(y,1))
+
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    print('test accuracy:', accuracy.eval(feed_dict={x: mnist.test.images, y: mnist.test.labels}))
+
+
+# Tasks
+# 1.    Make sure your code runs
+# 2.    Change batch size to 100, 200, 500. Report your accuracy
+# 3.    Run for various number of iterations, 10, 20, 1000, 1000, 10,000. Report your accuracy
+# 4.    What is this code doing? What type of classifer is this?
